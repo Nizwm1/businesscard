99fc95366a275dbcfc26052db5a78c071d22c19a
Nizwm Borgoyari <borgoyarinizwm@gmail.com>
2022-05-03 03:47:34 +0530

    Third Commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,24 @@
 import React from "react";
+import twitter from "../images/twitter-icon.svg";
+import facebook from "../images/facebook-icon.svg";
+import instagram from "../images/instagram.svg";
+import github from "../images/github.svg";
 
 export default function Footer() {
   return (
     <footer>
-      <img />
-      <img />
-      <img />
-      <img />
-      <img />
+      <a className="icons-twitter" href="#">
+        <img src={twitter} />
+      </a>
+      <a className="icons-facebook" href="#">
+        <img src={facebook} />
+      </a>
+      <a className="icons-instagram" href="#">
+        <img src={instagram} />
+      </a>
+      <a className="icons-gitub" href="#">
+        <img src={github} />
+      </a>
     </footer>
   );
 }
